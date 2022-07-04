2df5e1bf1199304a1fc5f837d4da271704265156
Raihan <raihan.muhammad91@ui.ac.id>
2022-07-04 16:46:21 +0700

    simple take off command

---
+++ include/vtol_offb/offb_node.h
@@ -0,0 +1,11 @@
+#ifndef OFFB_NODE_H_
+#define OFFB_NODE_H_
+
+#include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <mavros_msgs/CommandBool.h>
+#include <mavros_msgs/SetMode.h>
+#include <mavros_msgs/State.h>
+
+
+#endif // OFFB_NODE_H_
